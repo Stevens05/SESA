47b515a03d43602756748a30694682f51baf8336
stevens05 <jeanstevetamo@yahoo.fr>
2025-05-15 19:06:36 +0200

    Prometheus/Grafana configuration

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from fastapi import FastAPI, UploadFile, File#, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 import joblib, shutil, os
 import tempfile
 import librosa
 import python_multipart
 from utils.audio_features import extract_features
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 model = joblib.load("model/best_multiclass_model.pkl")
 scaler = joblib.load("model/scaler.pkl")
 le = joblib.load("model/label_encoder.pkl")
+
+
+# ✅ Instrumentation Prometheus
+Instrumentator().instrument(app).expose(app, include_in_schema=False, should_gzip=True)
 
 
 app.add_middleware(
@@ -20,9 +26,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"Hello": "world"}
+    return FileResponse("index.html")
 
 @app.post("/predict/")
 async def predict_audio(file: UploadFile = File(...)):
